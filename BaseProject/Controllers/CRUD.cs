7283bec459342382e72c6e01730a21eb9de6ff5a
selvinmedina <selvinmedina0016@gmail.com>
2020-06-14 17:12:27 -0600

    General files

---
+++ BaseProject/Controllers/CRUD.cs
@@ -0,0 +1,184 @@
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using BaseProject.Attribute;
+
+namespace BaseProject.Example
+{
+    public class CRUD
+    {
+        [SessionManager()]
+        public class TablaController : Controller
+        {
+            private Model db = new Model();
+
+            // GET: Tabla
+            public ViewResult Index() => View();
+
+            //Get data for update table
+            public async Task<JsonResult> GetData() =>
+                // Return JSON data from table Tabla
+                Json(
+                    new
+                    {
+                    // Async get data
+                    data = await Task.Run(() => db. // Get data from db
+                          tabla
+                          .OrderByDescending(x => x.cat_FechaCrea) // Order by descending Fecha Crea
+                          .Select(
+                              x => new
+                              {
+                                  descripcion = x.descripcion,
+                                  numero = x.cat_Id
+                              }
+                          )
+                        )
+                    },
+                    JsonRequestBehavior.AllowGet
+                    );
+
+            // GET: Tabla/Details/5
+            public async Task<JsonResult> Details(int? id)
+            {
+                if (id == null)
+                {
+                    return Json("error", JsonRequestBehavior.AllowGet);
+                }
+                tabla vTabla = await db.tabla.FindAsync(id);
+
+                if (vTabla == null)
+                {
+                    return Json("error", JsonRequestBehavior.AllowGet);
+                }
+
+                object result = new
+                {
+                    id = vTabla.cat_Id,
+                    descripcion = vTabla.descripcion,
+                    usuarioCrea = vTabla.tbUsuarios.usu_NombreDeUsuario,
+                    fechaCrea = General.FechaRetorno(vTabla.cat_FechaCrea),
+                    usuarioModifica = General.UsuarioModifica(vTabla.cat_UsuarioModifica, vTabla?.tbUsuarios1?.usu_NombreDeUsuario),
+                    fechaModifica = General.FechaModifica(vTabla.cat_FechaModifica)
+                };
+
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+
+            // POST: Tabla/Create
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public async Task<JsonResult> Create([Bind(Include = "descripcion")] tabla tabla)
+            {
+                string response = "error";
+
+                if (tabla.descripcion != null)
+                {
+                    try
+                    {
+                        if ( // Si se inserto mal retornar la vista
+                            await Task.Run(() =>
+
+                                // Retornar la insersion asincrona
+
+                                db.
+                                UDP_Esquema_vTabla_Insert(
+                                    tabla.descripcion,
+                                    1)
+                                .FirstOrDefault().Id == -1
+                            )
+                            )
+                        {
+                            return Json(response, JsonRequestBehavior.AllowGet);
+                        }
+                        else
+                        {
+                            //Si se inserto bien redireccionar al index
+                            response = "bien";
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        return Json(response, JsonRequestBehavior.AllowGet);
+                    }
+                }
+
+                return Json(response, JsonRequestBehavior.AllowGet);
+            }
+
+            // POST: Tabla/Edit/5
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public async Task<JsonResult> Edit([Bind(Include = "cat_Id,descripcion")] tabla tabla)
+            {
+                string response = "error";
+
+                if (tabla.cat_Id != 0 && tabla.descripcion != null)
+                {
+                    try
+                    {
+                        if ( // Si se inserto mal retornar la vista
+                            await Task.Run(() =>
+
+                                 // Retornar la insersion asincrona
+                                 db.
+                                UDP_Esquema_vTabla_Update(
+                                    tabla.cat_Id,
+                                    tabla.descripcion,
+                                    1)
+                                .FirstOrDefault()
+                                .Id == -1
+                            )
+                            )
+                        {
+                            return Json(response, JsonRequestBehavior.AllowGet);
+                        }
+                        else
+                        {
+                            //Si se inserto bien redireccionar al index
+                            response = "bien";
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        return Json(response, JsonRequestBehavior.AllowGet);
+                    }
+                }
+
+                return Json(response, JsonRequestBehavior.AllowGet);
+            }
+
+            // POST: Tabla/Delete/5
+            [HttpPost, ActionName("Delete")]
+            [ValidateAntiForgeryToken]
+            public async Task<JsonResult> DeleteConfirmed(int id)
+            {
+                string response = "error";
+                try
+                {
+                    tabla tabla = await db.tabla.FindAsync(id);
+                    db.tabla.Remove(tabla);
+                    await db.SaveChangesAsync();
+                    response = "bien";
+                }
+                catch (Exception)
+                {
+                    return Json(response, JsonRequestBehavior.AllowGet);
+                }
+                return Json(response, JsonRequestBehavior.AllowGet);
+            }
+
+            protected override void Dispose(bool disposing)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+                base.Dispose(disposing);
+            }
+        }
+    }
+}*/
---
+++ BaseProject/Helpers/General.cs
@@ -0,0 +1,123 @@
+﻿
+using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace PasteleriaShadday.Helpers
+{
+    public static class General
+    {
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(ConfigurationManager.ConnectionStrings["Connection"].ConnectionString);
+        }
+
+        // Hacer facilmente el retorno de la fecha
+        public static string FechaRetorno(DateTime fecha) =>
+            (fecha.Year + "/" + fecha.Month.ToString("d2") + "/" + fecha.Day.ToString("d2")).ToString();
+
+        // Hacer facilmente la fecha modifica de cada registro
+        public static string FechaModifica(DateTime? fecha) =>
+            fecha is null ? "No modificado" : FechaRetorno(fecha.Value);
+
+        // Hacer facilmene el usuario modifica
+        public static string UsuarioModifica(int? usuario, string nombreUsuario) =>
+            usuario is null ? "No modificado" : nombreUsuario;
+
+        // Crear un nombre unico con el parametro de la cantidad de caracteres del nombre
+        public static string CrateUniqueName(int length)
+        {
+            string _allowedChars = "abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ";
+            Byte[] randomBytes = new Byte[length];
+            char[] chars = new char[length];
+            int allowedCharCount = _allowedChars.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                Random randomObj = new Random();
+                randomObj.NextBytes(randomBytes);
+                chars[i] = _allowedChars[(int)randomBytes[i] % allowedCharCount];
+            }
+
+            return new string(chars);
+        }
+
+        // Crear nombre unico para una imagen
+        public static string CrateUniqueImageName(int length, string imageName)
+        {
+            return CrateUniqueName(10) + DateTime.Now.ToString("yyyyMMddHHmmss") + Path.GetExtension(imageName);
+        }
+    }
+
+    public static class CollectionExtensions
+    {
+        public static IQueryable<TSource> WhereIf<TSource>(
+            this IQueryable<TSource> source,
+            bool condition,
+            Expression<Func<TSource, bool>> predicate)
+        {
+            if (condition)
+                return source.Where(predicate);
+            else
+                return source;
+        }
+    }
+
+    // Subir una imagen
+    public class UploadImage
+    {
+        // El mensaje de error o exito que se enviara a la vista
+        private string ErrorMessage { get; set; } = "Se ha guardado éxitosamente";
+
+        // El tamaño del archivo
+        public decimal filesize { get; set; }
+
+        // Método para validar una imagen
+        /*
+         image: La imagen que se subió del servidor
+         nombreOriginal: Si fue editado entonces posiblemente tenga un nombre
+        */
+        public (string, int) ValidationImage(HttpPostedFileBase image, string nombreOriginal)
+        {
+            try
+            {
+                // Si hay un nombre original entonces que retorne un mensaje de éxito, pero que no suba la imagen
+                if (image == null && nombreOriginal != null && nombreOriginal != "")
+                {
+                    return (ErrorMessage, 3);
+                }
+
+                // Array de los tipos soportados para ina imagen
+                string[] supportedTypes = new[] { "image/png", "image/jpeg", "image/jpg", "image/gif" };
+
+                // Validar que sea el tipo soportado
+                if (!supportedTypes.Contains(image.ContentType))
+                {
+                    ErrorMessage = "La extensión del archivo no es válida - Solo se admiten archivos PNG/JPEG/JPG/GIF";
+                    return (ErrorMessage, 2);
+                }
+
+                // Si la imagen es más grande que el tamaño máximo, el tamaño es de MB
+                else if (image.ContentLength > ((filesize * (1000) * 1024)))
+                {
+                    ErrorMessage = "El tamaño máximo del archivo debe ser " + filesize + "MB";
+                    return (ErrorMessage, 2);
+                }
+                else
+                {
+                    // retornar que se puede subir la imagen, y un mensaje de éxito
+                    return (ErrorMessage, 1);
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = "No ha subido una imagen, contacte al administrador si esto es un error.";
+                return (ErrorMessage, 2);
+            }
+        }
+    }
+}
